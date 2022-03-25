ddf622dbdaf860edd59e9b3ce25bcc3842463625
Chris83 <83odie@gmail.com>
2022-03-25 19:21:46 -0400

    Added clear button and custom grid sizes 1-100

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-function BuildGrid(width, height){
-   const surfaceDiv = document.querySelector(".surface");
+function ClearSurface() {
+   const pixels = document.querySelectorAll(".pixel");
+   const gridSize = Math.sqrt(pixels.length);
 
-   for(let x=0; x<width; x++){
-      for(let y=0; y<height; y++){
+   const newGridSize = ClampValue(1, 100, +window.prompt("Enter a grid size (1 - 100):", gridSize));
+  
+   if(!newGridSize || newGridSize === gridSize){
+      pixels.forEach(pixel => (pixel.classList.contains("colored") ? pixel.classList.remove("colored") : null));
+   }
+   else{
+      document.querySelector(".surface").innerHTML = "";
+      BuildGrid(newGridSize);
+   }
+}
+
+function ClampValue(min, max, value){
+   if(isNaN(value)) return false;
+   if(value < min) return min;
+   if(value > max) return max;
+   return value;
+}
+
+function BuildGrid(gridSize){
+   const surfaceDiv = document.querySelector(".surface");
+   const surfaceSize = 880;
+   const pixelSize = Math.floor(surfaceSize/gridSize - 4);
+
+   for(let x=0; x<gridSize; x++){
+      for(let y=0; y<gridSize; y++){
          let pixel = document.createElement("div");
          pixel.classList.add("pixel");
+         pixel.style.minWidth =  pixelSize+"px";
+         pixel.style.minHeight = pixelSize+"px";
          pixel.addEventListener("mouseover", ColorPixel);
 
          surfaceDiv.appendChild(pixel);
@@ -16,4 +42,4 @@ function ColorPixel(){
    return (!this.classList.contains("colored") ? this.classList.add("colored") : null);
 }
 
-BuildGrid(16, 16);
+BuildGrid(10);
